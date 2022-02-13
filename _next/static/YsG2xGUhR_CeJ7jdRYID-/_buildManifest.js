b8271024ff4625a2c4ec3c65e804ebab22b9256f
ryanmondigotorino <53146680+ryanmondigotorino@users.noreply.github.com>
2022-02-13 12:03:46 +0000

    Deploying to gh-pages from @ ryanmondigotorino/ryanmondigotorino.github.io@19e0c83791b0ca6b2fa14f7ad50d33bed27c4da2 🚀

---
+++ _next/static/YsG2xGUhR_CeJ7jdRYID-/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/245-6974741e2f24f95d.js","static/chunks/pages/index-af6fb2598072e47e.js"],"/_error":["static/chunks/pages/_error-0260c064403af775.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
